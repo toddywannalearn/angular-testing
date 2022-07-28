d236e6fdfd45d65ed6b5b8a60646b06019ecf5a1
Douglas <toddynotes@gmail.com>
2022-07-28 20:18:33 -0300

    feat (counter, eslint): implemented the counter and installed the eslint

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+
+  currentValue: number = 0;
+
+  @Output()
+  countChange = new EventEmitter<number>();
+
+  constructor() { }
+
+  increment(): void {
+    this.currentValue++;
+    this.notify();
+  }
+
+  decrement(): void {
+    this.currentValue--;
+    this.notify();
+  }
+
+  reset(inputValue: any): void {
+    let value = parseInt(inputValue);
+    if(value != null){
+      this.currentValue = value; 
+      this.notify();
+    }
+  }
+
+  private notify(): void {
+    this.countChange.emit(this.currentValue);
+  }
+
+}
