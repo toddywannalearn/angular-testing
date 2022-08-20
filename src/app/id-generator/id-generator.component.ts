7466ed6c30117a9447b7b223b46dd1529f2bb16d
Douglas <toddynotes@gmail.com>
2022-08-20 15:41:21 -0300

    fix (id-generator): lint fix

--- src/app/id-generator/id-generator.component.ts
+++ src/app/id-generator/id-generator.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-id-generator',
   templateUrl: './id-generator.component.html',
   styleUrls: ['./id-generator.component.scss']
 })
-export class IdGeneratorComponent implements OnInit {
+export class IdGeneratorComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
